4223904599b001ffef2c77513d4c0d13e3d251cc
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-02-28 23:50:01 +0100

    Jo

--- communicator-broadcastchannel/js/src/main/scala/loci/communicator/ws/webnative/BroadcastChannelConnector.scala
+++ communicator-broadcastchannel/js/src/main/scala/loci/communicator/ws/webnative/BroadcastChannelConnector.scala
@@ -22,102 +22,48 @@ private class BroadcastChannelConnector[P <: BroadcastChannel: BroadcastChannelP
     extends Connector[P] {
 
   protected def connect(connectionEstablished: Connected[P]) = {
-
-    dom.window.addEventListener()
-
     val bc = new JSBroadcastChannel(name)
-
-    socket.onopen = { (_: dom.Event) =>
     
-      implicitly[BroadcastChannelProtocolFactory[P]].make(name) match {
-        case Failure(exception) =>
-          connectionEstablished.set(Failure(exception))
+    implicitly[BroadcastChannelProtocolFactory[P]].make(name) match {
+      case Failure(exception) =>
+        connectionEstablished.set(Failure(exception))
 
-        case Success(ws) =>
+      case Success(ws) =>
 
-          // heartbeat
+        val doClosed = Notice.Steady[Unit]
+        val doReceive = Notice.Stream[MessageBuffer]
 
-          var timeoutHandle: SetTimeoutHandle = null
-          var intervalHandle: SetIntervalHandle = null
+        val connection = new Connection[P] {
+          val protocol = ws
+          val closed = doClosed.notice
+          val receive = doReceive.notice
 
-          val resetTimeout = Notice.Stream[Unit]
-          val resetInterval = Notice.Stream[Unit]
-
-
-          // connection interface
-
-          val doClosed = Notice.Steady[Unit]
-          val doReceive = Notice.Stream[MessageBuffer]
-
-          val connection = new Connection[P] {
-            val protocol = ws
-            val closed = doClosed.notice
-            val receive = doReceive.notice
-
-            def open = socket.readyState == dom.WebSocket.OPEN
-            def send(data: MessageBuffer) = {
-              socket.send(data.backingArrayBuffer)
-              resetInterval.fire()
-            }
-            def close() = socket.close()
+          def open = bc.readyState == dom.WebSocket.OPEN
+          def send(data: MessageBuffer) = {
+            bc.send(data.backingArrayBuffer)
+            resetInterval.fire()
           }
+          def close() = bc.close()
+        }
 
-          connectionEstablished.set(Success(connection))
+        connectionEstablished.set(Success(connection))
 
+        bc.onmessage = { (event: dom.MessageEvent) =>
+          event.data match {
+            case data: ArrayBuffer =>
+              doReceive.fire(MessageBuffer wrapArrayBuffer data)
 
-          // heartbeat
+            case data: dom.Blob =>
+              val reader = new dom.FileReader
+              reader.onload = { (event: dom.Event) =>
+                doReceive.fire(MessageBuffer wrapArrayBuffer
+                  event.target.asInstanceOf[js.Dynamic].result.asInstanceOf[ArrayBuffer])
+              }
+              reader.readAsArrayBuffer(data)
 
-          resetTimeout.notice foreach { _ =>
-            if (timeoutHandle != null)
-              clearTimeout(timeoutHandle)
-            timeoutHandle = setTimeout(properties.heartbeatTimeout) {
-              connection.close()
-            }
+            case _ =>
           }
-
-          resetInterval.notice foreach { _ =>
-            if (intervalHandle != null)
-              clearInterval(intervalHandle)
-            intervalHandle = setInterval(properties.heartbeatDelay) {
-              socket.send("\uD83D\uDC93")
-            }
-          }
-
-          resetTimeout.fire()
-          resetInterval.fire()
-
-
-          // socket listeners
-
-          socket.onmessage = { (event: dom.MessageEvent) =>
-            event.data match {
-              case data: ArrayBuffer =>
-                doReceive.fire(MessageBuffer wrapArrayBuffer data)
-
-              case data: dom.Blob =>
-                val reader = new dom.FileReader
-                reader.onload = { (event: dom.Event) =>
-                  doReceive.fire(MessageBuffer wrapArrayBuffer
-                    event.target.asInstanceOf[js.Dynamic].result.asInstanceOf[ArrayBuffer])
-                }
-                reader.readAsArrayBuffer(data)
-
-              case _ =>
-            }
-            resetTimeout.fire()
-          }
-
-          socket.onclose = { (event: dom.CloseEvent) =>
-            clearInterval(intervalHandle)
-            clearTimeout(timeoutHandle)
-            doClosed.set()
-          }
-      }
-    }
-
-    socket.onerror = { (event: dom.Event) =>
-      connectionEstablished.trySet(Failure(new ConnectionException("Websocket failed to connect")))
-      socket.close()
+        }
     }
   }
 }
