01f6a70b4621a35221905993411c75dd7fbc83bc
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2023-02-28 23:07:04 +0100

    tmp

--- communicator-broadcastchannel/jvm/src/main/scala/loci/communicator/ws/webnative/package.scala
+++
@@ -1,7 +0,0 @@
-package loci
-package communicator
-package ws
-
-package object broadcastchannel {
-  private[broadcastchannel] def unavailable = sys.error("BroadcastChannel only available in JavaScript")
-}
